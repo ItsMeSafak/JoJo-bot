cf9aaf40a66a70ef183fa3aaba673ed07d6161fb
Safak <safak.inan61@hotmail.com>
2019-10-02 23:25:19 +0200

    gitignore

--- bot.js
+++ bot.js
@@ -28,6 +28,7 @@ var kira = "My name is Yoshikage Kira. I'm 33 years old. My house is in the nort
     "Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. " +
     "I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night." +
     " That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.";
+this si just bullshit lmaowsss
 
 var dioList = ["Oh? You're Approaching Me?", "WRYYYYYYYYYYY!!!", "MUDA MUDA MUDA MUDAAAAAAA!!", "ZA WARUDO!", "But it was me, Dio!"];
 
