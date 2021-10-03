b895938aa3197dd7ca2c77a3d9be5c19e58f5fe2
Safak Inan <safak.inan61@hotmail.com>
2021-10-03 16:58:32 +0200

    fuck me!

--- bot.js
+++ bot.js
@@ -1,17 +1,14 @@
-var Discord = require('discord.io');
+const { Client, Intents} = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: process.env.token,
-    autorun: true
-});
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 //Help List
 var helpCommand = "JoJo";
 var commands = ["`Yoshikage`", "`Dio`", "`Jotaro`"];
-var list = "Yare yare.. So you wanna make use of me? Just type in `" + helpCommand + "` followed by a command from below.\n\n" +
-    "These are my commands:\n";
-list += commands;
+var explanation = "Yare yare.. So you wanna make use of me? Just type in `" + helpCommand + "` followed by a command from below.\n\n" +
+    "These are my commands:\n" + commands;
 
 //Command values
 var kiraList = ["My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. " +
@@ -21,51 +18,36 @@ var kiraList = ["My name is Yoshikage Kira. I'm 33 years old. My house is in the
     "Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. " +
     "I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night." +
     " That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.", "KILLER QUEEEEEN! BITEESSS ZAAA DUSTOOO", "I-I'm having an erection..!"];
-
 var dioList = ["Oh? You're Approaching Me?", "WRYYYYYYYYYYY!!!", "MUDA MUDA MUDA MUDAAAAAAA!!", "ZA WARUDO!", "But it was me, Dio!", "8 seconds have passed."];
-
 var jotaroList = ["Yare yare daze...", "ORA ORA ORA ORA ORRRAAAA!!", "Star Platinum! ||Za Warudo!||", "I can't beat the shit out of you without gettign closer"];
 
 function getRndmLine(list){
     return list[Math.floor(Math.random() * list.length)]
 }
 //Bot going online
-bot.on('ready', function (evt) {
+bot.on("ready", function (evt) {
     console.log('Yare yare daze.. You are connected!');
 });
 
-//Main switch statement for execution fo commands
-bot.on('message', function (user, userID, channelID, message, evt) {
-    //Alternative BELOW
-    // console.log(message.substring(6).split(' '));
-    // console.log(message.substring(0,5).substring(5).split(' ')[0]);
-    if (message.substring(0, 4) === helpCommand) {
-        var cmd = message.substring(5);
+// //Main switch statement for execution fo commands
+bot.on("messageCreate", function (message) {
+    if (message.content.substring(0, 4) === helpCommand) {
+        var cmd = message.content.substring(5);
         switch (cmd) {
             case 'Yoshikage':
-                bot.sendMessage({
-                    to: channelID,
-                    message: getRndmLine(kiraList)
-                });
+                message.channel.send(getRndmLine(kiraList));
                 break;
             case 'Dio':
-                bot.sendMessage({
-                    to: channelID,
-                    message: getRndmLine(dioList)
-                });
+                message.channel.send(getRndmLine(dioList));
                 break;
             case 'Jotaro':
-                bot.sendMessage({
-                    to: channelID,
-                    message: getRndmLine(jotaroList)
-                });
+                message.channel.send(getRndmLine(jotaroList));
                 break;
             case 'help':
-                bot.sendMessage({
-                    to: channelID,
-                    message: list
-                });
+                message.channel.send(explanation);
                 break;
         }
     }
 });
+
+bot.login(process.env.DISCORD_KEY);
