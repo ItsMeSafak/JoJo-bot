9902260afdfe8de415ebafece75fb598bdc8d643
Safak Inan <safak.inan61@hotmail.com>
2021-10-05 08:56:59 +0200

    leave when finished

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const DisTube = require('distube');
 dotenv.config();
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-const distube = new DisTube(bot, { searchSongs: true, emitNewSongOnly: true, leaveOnEmpty: true })
+const distube = new DisTube(bot, { searchSongs: true, emitNewSongOnly: true, leaveOnEmpty: true, leaveOnFinish: true })
 
 //Help List
 var helpCommand = "jojo";
